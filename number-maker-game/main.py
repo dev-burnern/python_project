4305323dfe39ebb65b97e5e59d1b2eefa16737fb
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-06-07 17:14:43 +0900

    feat : 랜덤 숫자 맞추기 기능 추가

--- number-maker-game/main.py
+++ number-maker-game/main.py
@@ -1,10 +1,24 @@
-# 임포트 랜덤 모듈
+import random
 
-# 랜덤 넘버 생성 변수
+random_number = random.randint(1, 100)  # 랜덤 넘버 생성 변수
 
-# 게임 카운트 변수
+game_count = 1 # 게임 카운트 변수
 
 # 무한 반복
-# Try 블록 시작
-# 조건 작성
-# 예외 처리
+while True:
+    try:
+        my_number = int(input("1과 100 사이의 숫자를 입력하세요: "))
+        
+        # 조건 작성
+        if my_number > random_number:
+            print("다운")
+        elif my_number < random_number:
+            print("업")
+        else:
+            print(f"축하합니다! {game_count}번 만에 맞추셨습니다.")
+            break
+        
+        game_count += 1  # 게임 카운트 증가
+
+    except ValueError:
+        print("유효한 숫자를 입력하세요.") # 예외 처리
