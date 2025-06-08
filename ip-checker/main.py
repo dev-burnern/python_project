eaac804c1fac93552685df5087a43471e764930f
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-06-08 17:49:10 +0900

    feat : test 코드 추가 및 주석 처리

--- ip-checker/main.py
+++ ip-checker/main.py
@@ -2,9 +2,10 @@ import socket  # 소켓을 사용하여 IP 주소를 확인하기 위한 모듈
 import requests  # HTTPS를 사용하기 위한 requests 모듈
 # import re  # 정규 표현식을 사용하기 위한 re 모듈
 
-# input_addr = input("IP 주소를 입력하세요: ")  # 사용자로부터 IP 주소 입력 받기
+input_addr = input("IP 주소를 입력하세요: ")  # 사용자로부터 IP 주소 입력 받기
 
 in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 주소를 사용하기 위한 소켓 생성
+# in_addr.connect((input_addr, 443))  # 구글 서버에 연결하여 IP 주소를 확인
 in_addr.connect(('www.google.co.kr', 443))  # 구글 서버에 연결하여 IP 주소를 확인
 print("내부 IP 주소:", in_addr.getsockname()[0])  # 연결된 소켓의 IP 주소 출력
 
